a304d92eb4bba47ebd42064febdace267e7ba9d0
Ryan Kim <iawal86@gmail.com>
2020-10-25 16:35:23 -0700

    added redux for count

--- src/index.js
+++ src/index.js
@@ -1,24 +1,45 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const remove = document.getElementById("remove");
 const number = document.querySelector("span");
 
-let count = 0;
+const reducer = (count = 0, action) => {
+  console.log(count, action);
 
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "REMOVE") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
-const handleAdd = () => {
-  count++;
-  updateText();
-};
+const countStore = createStore(reducer);
 
-const handleRemove = () => {
-  count--;
-  updateText();
-};
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "REMOVE" });
 
-add.addEventListener("click", handleAdd);
-remove.addEventListener("click", handleRemove);
+// let count = 0;
+
+// number.innerText = count;
+
+// const updateText = () => {
+//   number.innerText = count;
+// };
+
+// const handleAdd = () => {
+//   count++;
+//   updateText();
+// };
+
+// const handleRemove = () => {
+//   count--;
+//   updateText();
+// };
+
+// add.addEventListener("click", handleAdd);
+// remove.addEventListener("click", handleRemove);
